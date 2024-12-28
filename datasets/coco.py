e9d3fcb5a659f329247033f0cf70bc8d7a334480
Gabrielbjb <111344809+Gabrielbjb@users.noreply.github.com>
2024-12-29 01:15:22 +0700

    Update coco.py

--- datasets/coco.py
+++ datasets/coco.py
@@ -123,27 +123,15 @@ def make_coco_transforms(image_set):
 
     if image_set == 'train':
         return T.Compose([
-            '''T.RandomHorizontalFlip(),
-            T.RandomSelect(
-                T.RandomResize(scales, max_size=1333),
-                T.Compose([
-                    T.RandomResize([400, 500, 600]),
-                    T.RandomSizeCrop(384, 600),
-                    T.RandomResize(scales, max_size=1333),
-                ])
-            ),
-            '''
             normalize
         ])
 
     if image_set == 'val':
         return T.Compose([
-            '''T.RandomResize([800], max_size=1333),'''
             normalize
         ])
     if image_set == 'test':
         return T.Compose([
-            '''T.RandomResize([800], max_size=1333),'''
             normalize
         ])
     raise ValueError(f'unknown {image_set}')
